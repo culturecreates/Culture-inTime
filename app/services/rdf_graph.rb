1b16b42ceea3c3eaa48e28dec3430ecddf19d81d
Gregory Saumier-Finch <gregory@culturecreates.com>
2022-03-14 11:27:23 -0400

    using ?uri instead of ?production

--- app/services/rdf_graph.rb
+++ app/services/rdf_graph.rb
@@ -43,18 +43,6 @@ class RDFGraph
   def self.spotlight(spotlight)
     entities = []
 
-    # query = SPARQL.parse(<<~SPARQL)
-    # SELECT * 
-    # WHERE { 
-    #   ?s <http://www.wikidata.org/prop/direct/P31> <http://www.wikidata.org/entity/Q7777570> ; 
-    #     <http://schema.org/description> ?description ; 
-    #     <http://www.wikidata.org/prop/direct/P1191> ?date . 
-    #     ?s <http://www.w3.org/2004/02/skos/core#prefLabel>  ?label  .
-    #     FILTER (lang(?description) = '#{I18n.locale}')
-    # }
-    # SPARQL
-    # results = query.execute(graph).to_json
-
     results =  artsdata_client.execute_sparql(generate_query_sparql(spotlight))
 
     results[:message].each do |e|
@@ -64,7 +52,7 @@ class RDFGraph
       startDate = e.dig("startDate","value") || ""
       place = e.dig("place","value") || ""
       image = e.dig("image","value") || ""
-      entity_uri = e.dig("production","value") || ""
+      entity_uri = e.dig("uri","value") || ""
       entities << Entity.new(title, description, startDate,  place, image, entity_uri)
     end
     entities
