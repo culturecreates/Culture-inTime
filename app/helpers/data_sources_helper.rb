50c46943e38ccafc1aa0f571e58af44f6ebf3d86
Gregory Saumier-Finch <gregory@culturecreates.com>
2022-04-21 12:39:06 -0400

    ObjectProperty

--- app/helpers/data_sources_helper.rb
+++ app/helpers/data_sources_helper.rb
@@ -2,7 +2,7 @@ module DataSourcesHelper
 
 
   def time_estimate(data_source)
-    estimate = data_source.uri_count.to_i * 0.005 * data_source.sample_graph.lines.count 
+    estimate = data_source.uri_count.to_i * 0.003 * data_source.sample_graph.lines.count 
     distance_of_time_in_words(Time.now,estimate.seconds.from_now )
   end
 
