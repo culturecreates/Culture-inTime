f9bc97879764817d06607fb459c4da7508ca6a5c
Gregory Saumier-Finch <gregory@culturecreates.com>
2022-04-25 18:01:09 -0400

    fix pagination

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
 
   # GET /
   def index
-    @most_viewed = Entity.spotlight(1)[1..5] ||= []
+    @most_viewed = Entity.spotlight(1).paginate()[1..5] ||= []
     @newly_added_spotlights = Spotlight.order(updated_at: :desc).limit(4)
   end
 
