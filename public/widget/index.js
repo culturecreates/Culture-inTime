a8a777b52e58883cd8c017f0143d8074d496ec1b
Gregory Saumier-Finch <gregory@culturecreates.com>
2023-01-30 19:45:31 -0500

    added public script

--- public/widget/index.js
+++ public/widget/index.js
@@ -1,33 +1,29 @@
+const widgetDiv = document.querySelector(".spotlight-widget");
+const limit = widgetDiv.dataset.limit;
+const endpoint = widgetDiv.dataset.endpoint;
+const locale = widgetDiv.dataset.locale;
+ const spotlight = widgetDiv.dataset.spotlight;
 
+const query = `${endpoint}/${locale}/search_rdf.json?spotlight=${spotlight}&limit=${limit}`; 
 
-  const widgetDiv = document.querySelector(".spotlight-widget");
-  const query = "<%= search_rdf_url(spotlight: @spotlight.id, format: :json, limit:5) %>"
+async function loadSpotlight() {
+  const res = await fetch(query);
+  const json = await res.json();
+  return json
+}
 
-  async function loadSpotlight() {
-    const res = await fetch(query);
-    const json = await res.json();
-    return json
-  }
-
-  loadSpotlight().then(entities => {
-     console.log(entities);
-     const ul=document.createElement('ul');
-     ul.classList.add("list-group");
-     entities.forEach((entity) => {
-      const length = 46;
-      const trimmedString = entity.description.length > length ? 
-                    entity.description.substring(0, length - 3) + "..." : 
-                    entity.description;
-      const li = document.createElement("li");
-      ul.appendChild(li);
-      // const im = document.createElement("img");
-      // im.src = entity.image;
-    
-      li.innerHTML=`<img src="${entity.image}"><div class="list-item"><div class="item-title">${entity.title}</div><div class="item-description">${trimmedString}</div></div>`;
-      // li.appendChild(im);
-
-    
-     
-    });
-     widgetDiv.appendChild(ul);
+loadSpotlight().then(entities => {
+   console.log(entities);
+   const ul=document.createElement('ul');
+   ul.classList.add("list-group");
+   entities.forEach((entity) => {
+    const length = 46;
+    const trimmedString = entity.description.length > length ? 
+                  entity.description.substring(0, length - 3) + "..." : 
+                  entity.description;
+    const li = document.createElement("li");
+    ul.appendChild(li);
+    li.innerHTML=`<img src="${entity.image}"><div class="list-item"><div class="item-title">${entity.title}</div><div class="item-description">${trimmedString}</div></div>`;
+  });
+   widgetDiv.appendChild(ul);
 });
