3689e3fb693660698b678c8437227e40bece15eb
Gregory Saumier-Finch <gregory@culturecreates.com>
2023-11-07 15:57:58 -0500

    check for frame

--- app/jobs/dump_spotlight_job.rb
+++ app/jobs/dump_spotlight_job.rb
@@ -11,9 +11,14 @@ class DumpSpotlightJob < ApplicationJob
       graph << entity.graph(approach: "wikidata")
       sleep(0.2) # part of a second
     end
-    
-    frame_json = JSON.parse(@spotlight.frame)
 
+    frame_json = nil
+    if @spotlight.frame.present?
+      begin
+        frame_json = JSON.parse(@spotlight.frame)
+      end
+    end
+    
     if frame_json.class == Hash
       puts "framing......"
       output = JSON::LD::API.frame( JSON.parse(graph.to_jsonld), frame_json)
